ba4a5c232c2c7a32a55e909d7d3cdd8ce16dc8a9
Robin Edwards <robin.ge@gmail.com>
2012-10-15 12:14:37 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup
 
 long_description = ""
 try:
-    long_description = open("README.md", "rb").read()
+    long_description = open("README.rst", "rb").read()
 except:
     pass
 
 
 setup(
     name="gitfeatures",
-    version="0.0.1",
+    version="0.0.2",
     packages=['gitfeatures'],
     license="MIT",
     long_description=long_description,
