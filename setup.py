5467c34d0c062a0f25a1ba889babdfef013ee53d
Martin Moss <martyzz1@gmail.com>
2023-06-16 18:06:05 +0100

    bump tag

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ long_description = readme
 
 setup(
     name="gitfeatures",
-    version="0.1.2",
+    version="0.1.3",
     packages=["gitfeatures"],
     license="MIT",
     long_description=long_description,
