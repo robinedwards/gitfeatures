d0e060fa2a7bc716d2d3af11100e1b1660712863
Martin Moss <martyzz1@gmail.com>
2024-09-17 11:45:32 +0100

    :bookmark: [ci skip] Bump version: 0.1.9 → 0.1.10

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ long_description = readme
 
 setup(
     name="gitfeatures",
-    version="0.1.9",
+    version="0.1.10",
     packages=["gitfeatures"],
     license="MIT",
     long_description=long_description,
